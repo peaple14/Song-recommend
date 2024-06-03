3fca81f9387e9851ebb1e381c11aa3e2b470f0c8
peaple14 <whwo1214@gmail.com>
2024-06-03 21:08:58 +0900

    화면 수정(김민규)

--- song-recommend/src/main/java/com/example/songrecommend/controller/MainController.java
+++ song-recommend/src/main/java/com/example/songrecommend/controller/MainController.java
@@ -19,15 +19,15 @@ public class MainController {
 
     @GetMapping("/")
     public String start(){
-//        //재테스트하기위해 보관
-//        SongDto songDto = new SongDto();
-//        songDto.setHighest_Note("F#3");
-//        songDto.setLowest_Note("C#5");
-//        System.out.println("들어왔을때:" + songDto);
-//        songDto = songService.change_int(songDto);
-//        System.out.println("들어간값:" + songDto);
-//        songDto.setHighest_Int(songDto.getHighest_Int());
-//        System.out.println("노래추천테스트:" + songService.recommendSong(songDto.getLowest_Int(), songDto.getHighest_Int(),1));
+        //재테스트하기위해 보관
+        SongDto songDto = new SongDto();
+        songDto.setHighest_Note("F#3");
+        songDto.setLowest_Note("C#5");
+        System.out.println("들어왔을때:" + songDto);
+        songDto = songService.change_int(songDto);
+        System.out.println("들어간값:" + songDto);
+        songDto.setHighest_Int(songDto.getHighest_Int());
+        System.out.println("노래추천테스트:" + songService.recommendSong(songDto.getLowest_Int(), songDto.getHighest_Int(),1));
         return "/vocal/index";
     }
 
@@ -51,7 +51,7 @@ public class MainController {
     public List<SongDto> getNote(@RequestBody SongDto songDto) {
         System.out.println("받은값" + songDto);
         songService.change_int(songDto);
-        List<SongDto> songList = songService.recommendSong(songDto.getLowest_Int(), songDto.getHighest_Int(),3); // 노래추천받기
+        List<SongDto> songList = songService.recommendSong(songDto.getLowest_Int(), songDto.getHighest_Int(),5); // 노래추천받기
         System.out.println(songList);
         return songList;
     }
